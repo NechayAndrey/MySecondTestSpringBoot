b9ab8dd443da9b240b5f4e0dc1f28eebea108f30
198019842005@mail.ru <198019842005@mail.ru>
2023-10-05 13:35:19 +0500

    Initial commit

---
+++ src/main/java/ru/arkhipov/MySecondTestSpringBoot/model/Request.java
@@ -0,0 +1,23 @@
+package ru.arkhipov.MySecondTestSpringBoot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Request {
+
+    private String uid;
+    private String operationUid;
+    private String systemName;
+    private String systemTime;
+    private String source;
+    private int communicationId;
+    private int templateId;
+    private int productCode;
+    private int smsCode;
+}
